b0a2750d699e69c5167e45fe53a98ae88d91f70d
Ka-me-nik <kamenik@utia.cas.cz>
2021-07-13 16:47:10 +0200

    Add files via upload

---
+++ matlab/tirfSimCentering.m
@@ -0,0 +1,379 @@
+% tirfSimCentering - GUI for final polishing of tracks exported by tirfSimGui
+%
+% Optional parameter:
+%    Folder - cell data folder ['' = will ask for the folder]
+%
+% Named parameters:
+%    Radius - cut-out radius (size = 2R+1) [10]
+%    Marker - displayed marker for CME track positions ['x']
+%    Color - cut-out overlay color [[.5 .5 1]]
+function tirfSimCentering(varargin)
+
+ip = inputParser;
+ip.addOptional('Folder', '', @ischar);
+ip.addParameter('Radius', 10, @(x)validateattributes(x,{'numeric'},{'positive','scalar','integer'}));
+ip.addParameter('Marker', 'x', @ischar);
+ip.addParameter('Color', [.5 .5 1], @(x) ischar(x) || (isnumeric(x) && length(x)==3));
+ip.parse(varargin{:});
+folder = ip.Results.Folder; % cell folder
+rCCP = ip.Results.Radius;   % cut-out radius (size = 2*r+1)
+marker = ip.Results.Marker; % displayed marker for CME track positions
+cMark = ip.Results.Color;   % cut-out overlay color
+
+rMark = 1000/65/2; % 1/4 um in TIRF-SIM resolution
+keyBegin = 'b';
+keyEnd = 'e';
+% tags
+tags = struct('name',{'split','ring'},'tag',{1,2},'key',{'x','o'},'marker',{'x','o'});
+
+if isempty(folder)
+    folder = uigetdir(pwd, 'Select the ''cell'' folder:');
+    if folder==0
+        return
+    end
+end
+if ~strcmp(folder(end), filesep)
+    folder = [folder filesep];
+end
+
+fn = [folder 'Tracking' filesep 'exportedTracks.mat'];
+assert(exist(fn,'file')==2,'Exported tracks data not found. Run ''tirfSimGui'' first.');
+expTrk = load(fn);
+chnl = expTrk.channels;
+nCh = length(chnl);
+ID = find([expTrk.tracks.status]>0);
+assert(~isempty(ID),'No exported tracks. Use ''tirfSimGui'' to export some.');
+if ~isfield(expTrk.tracks,'tag')
+    for i = 1:length(expTrk.tracks)
+        expTrk.tracks(i).tag = zeros(size(expTrk.tracks(i).f));
+    end
+end
+trk = expTrk.tracks(ID);
+changed = false(size(ID));
+% track correction (remove gaps/splitting/merging)
+err = find(cellfun(@(x)any(isnan(x)),{trk.f}));
+if ~isempty(err)
+    for i = err
+        f = min(trk(i).f):max(trk(i).f);
+        [uf,ii] = unique(trk(i).f);
+        ii = ii(~isnan(uf)); % ignore the NaN
+        [~,uf] = setdiff(f,uf);
+        while ~isempty(uf)
+            ii = ii([1:uf(1)-1,uf(1)-1:end]);
+            uf(1) = [];
+        end
+        trk(i).f = f;
+        trk(i).x = trk(i).x(ii);
+        trk(i).y = trk(i).y(ii);
+        trk(i).cx = trk(i).cx(ii);
+        trk(i).cy = trk(i).cy(ii);
+        trk(i).tag = trk(i).tag(ii);
+        changed(i) = true;
+    end
+    fprintf('Corrupted tracks repaired: %i\n', length(err));
+end
+% end track correction
+idx = 1;
+frm = cellfun(@(x)x(1),{trk.f});
+
+fprintf('Loading data ...');
+I = cell(1,nCh);
+mi = zeros(nCh,2);
+for i = 1:nCh
+    if strcmpi(chnl(i).movie(end-2:end),'mrc')
+        I{i} = rot90(ReadMRC([folder chnl(i).movie])).*reshape(chnl(i).bleaching,1,1,[]);
+    else
+        I{i} = loadTiff([folder chnl(i).movie]).*reshape(chnl(i).bleaching,1,1,[]);
+    end
+    mi(i,:) = [min(I{i}(:)),max(I{i}(:))];
+    I{i} = padarray(I{i},rCCP*[1,1]);
+end
+N = size(I{1},3);
+fprintf(' done.\n');
+
+% prepare masks (circles) for radial function computation
+C = zeros(2*rCCP+1,2*rCCP+1,2*rCCP);
+[x,y] = meshgrid(-rCCP-.495:.01:rCCP+.495);
+for r = 1:2*rCCP
+    tc = (x.^2+y.^2<(r/2)^2);
+    C(:,:,r) = reshape(mean(reshape(reshape(mean(reshape(tc,100,[])),2*rCCP+1,[]).',100,[])),size(C(:,:,1)));
+end
+for r = 2*rCCP:-1:2
+    C(:,:,r) = C(:,:,r)-C(:,:,r-1);
+end
+C = C./sum(sum(C));
+
+fprintf('Calculating intensities ...');
+A = cell(1,length(trk));
+tc = zeros([2*rCCP+1,2*rCCP+1,nCh]);
+for i = 1:length(trk)
+    for j = 1:length(trk(i).f)
+        for c = 1:nCh
+            tc(:,:,c) = I{c}(round(trk(i).y(j)+trk(i).cy(j))+(0:2*rCCP),round(trk(i).x(j)+trk(i).cx(j))+(0:2*rCCP),trk(i).f(j));
+            A{i}(j,c) = max(sum(sum(tc(:,:,c).*C)));
+        end
+    end
+end
+fprintf(' done.\n');
+
+f = figure('Name',sprintf('TIRF-SIM cut-out centering'),'Units','normalized','Position',[.04 .04 .9 .9],'Menubar','none','Toolbar','none','WindowKeyPressFcn',@keyPress,'CloseRequestFcn',@closeFig);
+if ~verLessThan('matlab','9.4')
+    f.WindowState = 'maximized';
+end
+subplot('Position',[0.01,0.03,0.63,0.96]);
+img = image(I{1}(rCCP+1:end-rCCP,rCCP+1:end-rCCP,frm(idx)));
+colormap(gray(256));
+img.ButtonDownFcn = @imgClick;
+set(gca,'CLim',mi(1,:));
+axis equal off
+zoom reset
+fi = frm(idx)-trk(idx).f(1)+1;
+rec = rectangle('Position',[trk(idx).x(fi)+trk(idx).cx(fi)-rCCP-0.5,trk(idx).y(fi)+trk(idx).cy(fi)-rCCP-0.5,2*rCCP+1,2*rCCP+1],'EdgeColor','g');
+subplot('Position',[0.65,0.03,0.23,0.4]);
+cut = image(zeros([2*rCCP+1,2*rCCP+1,3]));
+axis equal off
+zoom reset
+hold on
+tfi = 0:pi/50:2*pi;
+plot(rMark*cos(tfi)+rCCP+1,rMark*sin(tfi)+rCCP+1,':','Color',cMark);
+plot(rMark/2*cos(tfi)+rCCP+1,rMark/2*sin(tfi)+rCCP+1,':','Color',cMark);
+MD = plot(rCCP+1,rCCP+1,marker,'Color',cMark);
+% plot(rCCP+1,rCCP+1,'o','MarkerSize',100,'Color',[.5 .5 1]);
+line([.5,rCCP+1;2*rCCP+1.5,rCCP+1],[rCCP+1,.5;rCCP+1,2*rCCP+1.5],'Color',cMark,'LineStyle',':');
+ax(1) = subplot('Position',[0.65,0.455,0.23,0.2]);
+hold on
+RI = cell(1,2);
+RI{1} = plot((1:2*rCCP)/2,zeros(1,2*rCCP),'g');
+RI{2} = plot((1:2*rCCP)/2,zeros(1,2*rCCP),'r');
+ax(2) = subplot('Position',[0.65,0.68,0.23,0.2]);
+hold on
+TA = cell(1,2);
+TA{1} = plot(1:N,zeros(1,N),'g');
+TA{2} = plot(1:N,zeros(1,N),'r');
+TA{3} = line([1 1],[0,1],'Color',[.5 .5 .5]);
+for i = 1:length(tags)
+    TA{i+3} = scatter([],[],50,'b',tags(i).marker);
+end
+lst = uicontrol(f,'Style','listbox','String',arrayfun(@(x)sprintf('%3i   (%3i - %3i)',ID(x),trk(x).f(1),trk(x).f(end)),1:length(ID),'Uniform',0),'Units','normalized','Position',[0.89 0.03 0.1 0.96],'Value',idx,'Callback',@lstChange);
+txt = uicontrol(f,'Style','text','String',['Frame: ' num2str(frm(idx)) ' / ' num2str(N)],'Units','normalized','Position',[0 0 .05 .02]);
+tagTxt = [];
+for i = 1:length(tags)
+    tagTxt = sprintf('%s, %s - %s',tagTxt,tags(i).key,tags(i).name);
+end
+uicontrol(f,'Style','text','String',sprintf('Up / Down - select track\nLeft / Right - selet frame\nHome / End - select first / last frame\nShift + Arrows - shift cut-out\n%s / %s - set begin / end of track (+-1 frame)\nTags: %s',keyBegin,keyEnd,tagTxt(2:end)),'Units','normalized','Position',[0.64 0.89 0.14 0.1],'Max',2);
+uicontrol(f,'Style','pushbutton','String','Reset track to CME detection','Units','normalized','Position',[0.78 0.96 .1 .03],'Max',2,'Callback',@resetTrack);
+uicontrol(f,'Style','pushbutton','String','Resave all cut-outs','Units','normalized','Position',[0.78 0.92 .1 .03],'Callback',@resaveAll);
+chCh = uicontrol(f,'Style','checkbox','String','Show all channels','Value',0,'Units','normalized','Position',[0.78 0.885 .1 .03],'Callback',@showChannels);
+sld = uicontrol(f,'Style','slider','Min',1,'Max',N,'SliderStep',[1 1]./(N-1),'Value',frm(idx),'Units','normalized','Position',[.05 0 .95 .02],'Callback',@sldChange);
+sldChange(sld);
+
+function imgClick(obj,~)
+    p = obj.Parent.CurrentPoint(1,1:2);
+    eidx = find(arrayfun(@(x)any(x.f==frm(idx)),trk));
+    eidx2 = arrayfun(@(x)find(x.f==frm(idx),1),trk(eidx));
+    crd = [arrayfun(@(t,i)t.x(i)+t.cx(i),trk(eidx),eidx2);arrayfun(@(t,i)t.y(i)+t.cy(i),trk(eidx),eidx2)].';
+    d = sqrt(sum((crd-p).^2,2));
+    [d,di] = min(d);
+%     if (d<20)
+        lst.Value = eidx(di);
+        frm(eidx(di)) = frm(idx);
+        lstChange(lst);
+%     end
+end
+function sldChange(obj,~)
+    frm(idx) = round(get(obj,'Value'));
+    set(txt,'String',sprintf('Frame: %i / %i',frm(idx),N));
+    img.CData = I{1}(rCCP+1:end-rCCP,rCCP+1:end-rCCP,frm(idx));
+    posChange;
+    uicontrol(txt);
+end
+function posChange()
+    fi = frm(idx)-trk(idx).f(1)+1;
+    if fi<1 || fi>length(trk(idx).f)
+        rec.LineStyle = ':';
+        MD.MarkerIndices = [];
+    else
+        rec.LineStyle = '-';
+        MD.MarkerIndices = 1;
+    end
+    fi = max(min(fi,length(trk(idx).f)),1);
+    rec.Position = [trk(idx).x(fi)+trk(idx).cx(fi)-rCCP-0.5,trk(idx).y(fi)+trk(idx).cy(fi)-rCCP-0.5,rec.Position(3:4)];
+    showChannels();
+end
+function lstChange(obj,~)
+    idx = get(obj,'Value');
+    sld.Value = frm(idx);
+    sldChange(sld);
+end
+function keyPress(~,e)
+    fi = frm(idx)-trk(idx).f(1)+1;
+    if length(e.Modifier)==1 && strcmpi(e.Modifier{1},'shift')
+        if fi>=1 && fi<=length(trk(idx).f)
+            switch e.Key
+                case 'leftarrow'
+                    trk(idx).cx(fi) = trk(idx).cx(fi) - 1;
+                    changed(idx) = true;
+                    posChange;
+                case 'rightarrow'
+                    trk(idx).cx(fi) = trk(idx).cx(fi) + 1;
+                    changed(idx) = true;
+                    posChange;
+                case 'uparrow'
+                    trk(idx).cy(fi) = trk(idx).cy(fi) - 1;
+                    changed(idx) = true;
+                    posChange;
+                case 'downarrow'
+                    trk(idx).cy(fi) = trk(idx).cy(fi) + 1;
+                    changed(idx) = true;
+                    posChange;
+            end
+        end
+    else
+        switch e.Key
+            case 'leftarrow'
+                sld.Value = max(sld.Value-1,sld.Min);
+                sldChange(sld);
+            case 'rightarrow'
+                sld.Value = min(sld.Value+1,sld.Max);
+                sldChange(sld);
+            case 'home'
+                sld.Value = trk(idx).f(1);
+                sldChange(sld);
+            case 'end'
+                sld.Value = trk(idx).f(end);
+                sldChange(sld);
+            case 'uparrow'
+                lst.Value = max(idx-1,1);
+                lstChange(lst);
+            case 'downarrow'
+                lst.Value = min(idx+1,length(ID));
+                lstChange(lst);
+            case keyBegin
+                if frm(idx)==trk(idx).f(1)-1
+                    trk(idx).f = [frm(idx),trk(idx).f];
+                    trk(idx).x = [-1000,trk(idx).x];
+                    trk(idx).cx = [trk(idx).x(2)+trk(idx).cx(1)+1000,trk(idx).cx];
+                    trk(idx).y = [-1000,trk(idx).y];
+                    trk(idx).cy = [trk(idx).y(2)+trk(idx).cy(1)+1000,trk(idx).cy];
+                    trk(idx).tag = [0,trk(idx).tag];
+                    A{idx} = [0,0;A{idx}];
+                    lst.String{idx} = sprintf('%3i   (%3i - %3i)',ID(idx),trk(idx).f(1),trk(idx).f(end));
+                    posChange();
+                    changed(idx) = true;
+                elseif frm(idx)==trk(idx).f(1)+1
+                    trk(idx).f(1) = [];
+                    trk(idx).x(1) = [];
+                    trk(idx).cx(1) = [];
+                    trk(idx).y(1) = [];
+                    trk(idx).cy(1) = [];
+                    trk(idx).tag(1) = [];
+                    A{idx}(1,:) = [];
+                    lst.String{idx} = sprintf('%3i   (%3i - %3i)',ID(idx),trk(idx).f(1),trk(idx).f(end));
+                    posChange();
+                    changed(idx) = true;
+                end
+            case keyEnd
+                if frm(idx)==trk(idx).f(end)+1
+                    trk(idx).f = [trk(idx).f,frm(idx)];
+                    trk(idx).x = [trk(idx).x,-1000];
+                    trk(idx).cx = [trk(idx).cx,trk(idx).x(end-1)+trk(idx).cx(end)+1000];
+                    trk(idx).y = [trk(idx).y,-1000];
+                    trk(idx).cy = [trk(idx).cy,trk(idx).y(end-1)+trk(idx).cy(end)+1000];
+                    trk(idx).tag = [trk(idx).tag,0];
+                    A{idx} = [A{idx};0,0];
+                    lst.String{idx} = sprintf('%3i   (%3i - %3i)',ID(idx),trk(idx).f(1),trk(idx).f(end));
+                    posChange();
+                    changed(idx) = true;
+                elseif frm(idx)==trk(idx).f(end)-1
+                    trk(idx).f(end) = [];
+                    trk(idx).x(end) = [];
+                    trk(idx).cx(end) = [];
+                    trk(idx).y(end) = [];
+                    trk(idx).cy(end) = [];
+                    trk(idx).tag(end) = [];
+                    A{idx}(end,:) = [];
+                    lst.String{idx} = sprintf('%3i   (%3i - %3i)',ID(idx),trk(idx).f(1),trk(idx).f(end));
+                    posChange();
+                    changed(idx) = true;
+                end
+        end
+        if length(e.Key)==1 && fi>=1 && fi<=length(trk(idx).f)
+            keyIdx = find([tags.key]==e.Key,1);
+            if ~isempty(keyIdx)
+                trk(idx).tag(fi) = bitxor(trk(idx).tag(fi),tags(keyIdx).tag);
+                showChannels();
+                changed(idx) = true;
+            end
+        end
+    end
+end
+function resetTrack(~,~)
+    trk(idx).cx(:) = 0;
+    trk(idx).cy(:) = 0;
+    changed(idx) = true;
+    posChange;
+end
+function resaveAll(~,~)
+    changed(:) = true;
+end
+function showChannels(~,~)
+    fi = frm(idx)-trk(idx).f(1)+1;
+    cutI = zeros([2*rCCP+1,2*rCCP+1,nCh]);
+    for iii = 1:nCh
+        cutI(:,:,iii) = I{iii}(round(rec.Position(2)+rCCP+0.5)+(0:2*rCCP),round(rec.Position(1)+rCCP+0.5)+(0:2*rCCP),frm(idx));
+        if fi>=1 && fi<=length(trk(idx).f)
+            A{idx}(fi,iii) = max(sum(sum(cutI(:,:,iii).*C)));
+        end
+    end
+    RI{1}.YData = squeeze(sum(sum(cutI(:,:,1).*C)));
+    RI{2}.YData = squeeze(sum(sum(cutI(:,:,2).*C)));
+    TA{3}.YData = [min(min(A{idx}(:,1:2))),max(max(A{idx}(:,1:2)))];
+    TA{1}.XData = trk(idx).f;
+    TA{1}.YData = A{idx}(:,1);
+    TA{2}.XData = trk(idx).f;
+    TA{2}.YData = A{idx}(:,2);
+    TA{3}.XData = frm(idx)*[1,1];
+    for ti = 1:length(tags)
+        TA{ti+3}.XData = find(bitand(trk(idx).tag,tags(ti).tag))+trk(idx).f(1)-1;
+        TA{ti+3}.YData = TA{1}.YData(TA{ti+3}.XData-trk(idx).f(1)+1);
+    end
+    ax(2).YLimMode = 'auto';
+    ax(2).YLim(1) = 0;
+%     ax(2).YLim = [0,max(A{idx}(:))];
+    TA{3}.YData = ax(2).YLim;
+    ax(1).YLim = ax(2).YLim;
+    if fi>=1 && fi<=length(trk(idx).f)
+        MD.XData = trk(idx).x(fi)-round(trk(idx).x(fi))-trk(idx).cx(fi)+rCCP+1;
+        MD.YData = trk(idx).y(fi)-round(trk(idx).y(fi))-trk(idx).cy(fi)+rCCP+1;
+    end
+    if chCh.Value>0
+%         ti = (cutI-reshape(mi(:,1),1,1,[]))./(reshape(mi(:,2)-mi(:,1),1,1,[]));
+        ti = (cutI-ax(2).YLim(1))./diff(ax(2).YLim);
+        cut.CData = cat(3,ti(:,:,2),ti(:,:,1),zeros(2*rCCP+1));
+    else
+        cut.CData = cutI(:,:,1);
+    end
+end
+function closeFig(~,~)
+    if any(changed)
+        res = questdlg('Save changes?','Finish centering');
+        if strcmpi(res,'Yes')
+            expTrk.tracks(ID) = trk;
+            save([folder 'Tracking' filesep 'exportedTracks.mat'],'-struct','expTrk');
+            for it = find(changed)
+                for ch = 1:length(chnl)
+                    E = zeros([2*rCCP+1,2*rCCP+1,length(trk(it).f)],'single');
+                    for fr = 1:length(trk(it).f)
+                        E(:,:,fr) = I{ch}(round(trk(it).y(fr)+trk(it).cy(fr))+(0:2*rCCP),round(trk(it).x(fr)+trk(it).cx(fr))+(0:2*rCCP),trk(it).f(fr));
+                    end
+                    saveTiff(E,[folder 'Tracking' filesep 'Track_' num2str(ID(it)) '_' num2str(chnl(ch).wavelength) '.tif']);
+                end
+            end
+        elseif ~strcmpi(res,'No')
+            return;
+        end
+    end
+    delete(gcf);
+end
+
+end
---
+++ matlab/tirfSimGui.m
@@ -0,0 +1,312 @@
+% tirfSimGui - GUI for visualisation of TIRF-SIM data and tracks from CME analysis
+%
+% Parameters:
+%    folder - cell data folder ['' = will ask for the folder]
+%    boost - intensity scaling for TIRF-SIM data (hack for visual improvement) [4]
+%
+% Shows TIRF-SIM data as well as results of "classical CME analysis" and
+% enables manual selection of "interesting" (in any sense) tracks for
+% further processing.
+function tirfSimGui(folder,boost)
+
+marker = 'x';  % displayed marker for CME track positions
+rCCP = 10;     % cut-out radius (size = 2*r+1)
+simIdx = 0;    % 0 = mean, 1-9 = SIM index
+if nargin<2
+    boost = 4; % intensity scale
+end
+
+if nargin<1 || isempty(folder)
+    folder = uigetdir(pwd, 'Select the ''cell'' folder:');
+    if folder==0
+        return
+    end
+end
+if ~strcmp(folder(end), filesep)
+    folder = [folder filesep];
+end
+% detect framerate
+fr = regexp(folder, '_(\d+)?(\.)?\d+s', 'match');
+if ~isempty(fr)
+    framerate = str2double(fr{1}(2:end-1));
+else
+    fr = regexp(folder, '_\d+ms', 'match');
+    if ~isempty(fr)
+        framerate = str2double(fr{1}(2:end-2))/1000;
+    else
+        framerate = 2; % default: 2s
+    end
+end
+
+fn = dir([folder 'TIRF*_L.mrc']);
+if numel(fn)<1
+    error('Cannot find SIM reconstruction.');
+end
+wl = cellfun(@(s) str2double(s(5:7)),{fn.name});
+if wl(1)~=488
+    error('First (master) wavelength is not 488.');
+end
+nCh = numel(wl);
+N = 0;
+imgsz = [];
+fprintf('Loading image data ... ');
+I = cell(2,nCh);
+for c = 1:nCh
+    a = ReadMRC([folder fn(c).name(1:end-6) '.mrc']);
+    if c==1
+        N = size(a,3)/9;
+        imgsz = size(a(:,:,1));
+    else
+        if any(size(a)~=[imgsz,9*N])
+            error('Inconsistent input data size.\n');
+        end
+    end
+    a = single(reshape(a,size(a,1),size(a,2),9,[]));
+    if simIdx < 1
+        I{1,c} = rot90(squeeze(mean(a,3)));
+    else
+        I{1,c} = rot90(squeeze(a(:,:,simIdx,:)));
+    end
+end
+clear a
+fnr = dir([folder 'TIRF*_L*reg.tif']);
+if numel(fnr)>0
+    wlr = cellfun(@(s) str2double(s(5:7)),{fnr.name});
+    i = arrayfun(@(w) find(wl==w),wlr);
+    fn(i) = fnr;
+end
+fn = {fn.name};
+BC = zeros(N,nCh);
+for c = 1:nCh
+    if strcmpi(fn{c}(end-2:end),'mrc')
+        I{2,c} = rot90(ReadMRC([folder fn{c}]));
+    else
+        I{2,c} = loadTiff([folder fn{c}]);
+    end
+    if any(size(I{2,c})~=[2*imgsz,N])
+        erorr('Inconsistent reconstruction data sizes.');
+    end
+    b = double(squeeze(mean(mean(I{2,c},1),2)));
+    x = (1:N).';
+    ft = fit(x,b,'exp2');
+    b = ft.a*exp(ft.b*x)+ft.c*exp(ft.d*x);
+    BC(:,c) = mean(b)./b; % preserve mean value
+%     BC(:,c) = 1./b; % normalize mean to 1
+end
+movies = fn;
+m = zeros(N,nCh,2);
+for i = 1:nCh
+    m(:,i,1) = squeeze(min(min(I{1,i},[],1),[],2));
+    m(:,i,2) = squeeze(max(max(I{1,i},[],1),[],2));
+end
+% mI2 = [min(I{2,1}(:)),min(I{2,2}(:))];
+fprintf('done.\n');
+idx = 1;
+
+% fn = [data.source 'Detection' filesep 'detection_v2.mat'];
+% if exist(fn,'file')~=2
+%     error('Detection data not found.');
+% end
+% D = cell(1,N);
+% det = load(fn);
+% for i = 1:N
+%     D{i} = [2*det.frameInfo(i).x;2*det.frameInfo(i).y;det.frameInfo(i).A].';
+% end
+
+msk = imread([folder 'Detection' filesep 'cellmask.tif']);
+
+%load tracks
+fprintf('Loading tracks ... ');
+fn = [folder 'Tracking' filesep 'ProcessedTracks.mat'];
+if exist(fn,'file')~=2
+    error('Tracking data not found.');
+end
+trk = load(fn);
+trk = trk.tracks;
+% trk = trk([trk.nSeg]==1);
+[~,i] = sort([trk.lifetime_s],'descend');
+trk = trk(i);
+nt = numel(trk);
+% apply cell mask
+x = NaN(1,nt);
+y = NaN(1,nt);
+for t = 1:nt
+    x(t) = round(nanmean(trk(t).x(1,:)));
+    y(t) = round(nanmean(trk(t).y(1,:)));
+end
+trk = trk(msk(sub2ind(size(msk), y, x))==1);
+trk = trk([trk.lifetime_s] >= 5*framerate);
+nt = numel(trk);
+T = cell(1,N);
+for i = 1:N
+    tt = [];
+    for t = 1:nt
+        ii = find(trk(t).f==i);
+        if sum(ii)>0
+            tt = [tt [2*trk(t).x(ii);2*trk(t).y(ii);t*ones(size(ii))]];
+        end
+    end
+    T{i} = tt.';
+%     T{i} = [2*det.frameInfo(i).x;2*det.frameInfo(i).y;det.frameInfo(i).A].';
+end
+fprintf('done.\n');
+idxT = 0;
+
+fn = [folder 'Tracking' filesep 'exportedTracks.mat'];
+if exist(fn,'file')==2
+    expTrk = load(fn);
+    expTrk = expTrk.tracks;
+else
+    z = cellfun(@(x)zeros(size(x)),{trk.f},'Uniform',0);
+    expTrk = struct('f',{trk.f},'x',cellfun(@(x)2*x,{trk.x},'Uniform',0),'y',cellfun(@(x)2*x,{trk.y},'Uniform',0),'cx',z,'cy',z,'status',num2cell(zeros(size(z))));
+    et = dir([folder 'Tracking' filesep '*Track_*_' num2str(wl(1)) '.tif']);
+    en = cellfun(@(x)str2double(regexpi(x,'(?<=_)\d+(?=_\d{3})','match','once')),{et.name});
+    [expTrk(en).status] = deal(1);
+end
+
+S = cell(1,2);
+chST = cell(1,2);
+chCh = cell(nCh,2);
+img = cell(1,2);
+ax = zeros(1,2);
+f = figure('Name',sprintf('TIRF-SIM'),'Units','normalized','Position',[.04 .04 .9 .9],'Menubar','none','Toolbar','figure','WindowKeyPressFcn',@keyPress,'CloseRequestFcn',@closeFig);
+if ~verLessThan('matlab','9.4')
+    set(f,'WindowState','maximized');
+end
+for i = 1:2
+    chST{i} = uicontrol(f,'Style','checkbox','String',['Show track points (' num2str(i) ')'],'Units','normalized','Position',[0.02+(2-i)*0.49,0.02,0.2,0.02],'Callback',@chSDChange);
+    for j = 1:nCh
+        chCh{j,i} = uicontrol(f,'Style','checkbox','String',[num2str(wl(j)) ' (' num2str(i) ')'],'Units','normalized','Position',[0.1+j*0.1+(2-i)*0.49,0.02,0.2,0.02],'Callback',@chSDChange,'Value',1);
+    end
+    S{i} = subplot('Position',[0.02+(2-i)*0.49, 0.04, 0.47, 0.96]);
+    id = imresize(I{i,1}(:,:,idx),3-i);
+    id = id - min(id(:));
+    id = id/max(id(:));
+    img{i} = image(repmat(id,[1,1,3]));
+    set(img{i},'ButtonDownFcn',@imgClick);
+    axis equal off
+    ax(i) = img{i}.Parent;
+    zoom reset
+    hold on
+end
+linkaxes(ax,'xy');
+txt = uicontrol(f,'Style','text','String',['Frame: 1 / ' num2str(N)],'Units','normalized','Position',[0 0 .05 .02]);
+sld = uicontrol(f,'Style','slider','Min',1,'Max',N,'SliderStep',[1 1]./(N-1),'Value',idx,'Units','normalized','Position',[.05 0 .95 .02],'Callback',@sldChange);
+txtT = uicontrol(f,'Style','text','String','Track:','Units','normalized','Position',[0.4 .985 0.2 .015]);
+uicontrol(f,'Style','pushbutton','String','Export','Units','normalized','Position',[0.6 .98 0.1 .02],'Callback',@export);
+uicontrol(sld);
+L = cell(1,4);
+sldChange(sld);
+% waitfor(f); % wait till the form is closed
+
+function sldChange(obj,~)
+    idx = round(get(obj,'Value'));
+    set(txt,'String',sprintf('Frame: %i / %i',idx,N));
+    idata = zeros([2*imgsz,3,2]);
+    for k = 1:nCh
+        if chCh{k,1}.Value>0
+            tmp = imresize(I{1,k}(:,:,idx),2);
+            idata(:,:,4-k,1) = (tmp-m(idx,k,1))/(m(idx,k,2)-m(idx,k,1));
+        end
+        if chCh{k,2}.Value>0
+            tmp = I{2,k}(:,:,idx);
+            if boost==0
+                tmp = tmp-min(tmp(:));
+                tmp = tmp/max(tmp(:));
+            else
+                tmp = (boost*tmp-m(idx,k,1))/(m(idx,k,2)-m(idx,k,1));
+            end
+            idata(:,:,4-k,2) = tmp;
+        end
+    end
+    if nCh==1
+        idata(:,:,1:2,:) = repmat(idata(:,:,3,:),[1,1,2,1]);
+    end
+    img{1}.CData = idata(:,:,:,1);
+    img{2}.CData = idata(:,:,:,2);
+    for k = 1:4
+        if ~isempty(L{k})
+            delete(L{k});
+            L{k} = [];
+        end
+    end
+    if ~isempty(T{idx})
+        nat = T{idx}(T{idx}(:,3)~=idxT,:);
+        at = T{idx}(T{idx}(:,3)==idxT,:);
+        for k = 1:2
+            subplot(S{k});
+            if chST{k}.Value>0
+                L{k} = scatter(nat(:,1),nat(:,2),100,[1 1 1],marker,'ButtonDownFcn',@imgClick);
+                L{k}.MarkerEdgeAlpha = .5;
+            end
+            L{k+2} = scatter(at(:,1),at(:,2),100,[1 1 1],'s');
+            L{k+2}.MarkerEdgeAlpha = .5;
+        end
+        if idxT>0
+            set(txtT,'String',sprintf('Track: %i (%g / %g s)',idxT,(idx-trk(idxT).start+.5)*framerate,trk(idxT).lifetime_s));
+        else
+            set(txtT,'String','Track:');
+        end
+    end
+end
+function imgClick(obj,~)
+    p = get(obj.Parent,'CurrentPoint');
+    p = p(1,1:2);
+    d = sqrt(sum((T{idx}(:,1:2)-p).^2,2));
+    [d,di] = min(d);
+    if (d<20)
+        idxT = T{idx}(di,3);
+    else
+        idxT = 0;
+    end
+    chSDChange;
+end
+function chSDChange(~,~)
+    sldChange(sld);
+    uicontrol(sld);
+end
+function keyPress(~,e)
+    switch e.Key
+        case '1'
+            chST{1}.Value = 1-chST{1}.Value;
+            chSDChange;
+        case '2'
+            chST{2}.Value = 1-chST{2}.Value;
+            chSDChange;
+    end
+end
+function closeFig(~,~)
+    ss.tracks = expTrk;
+    for ci = 1:nCh
+        ss.channels(ci).movie = movies{ci};
+        ss.channels(ci).wavelength = wl(ci);
+        ss.channels(ci).bleaching = BC(:,ci);
+    end
+    save([folder 'Tracking' filesep 'exportedTracks.mat'],'-struct','ss');
+    delete(gcf);
+end
+function export(~,~)
+    if idxT>0
+        XY = -ones(trk(idxT).end-trk(idxT).start+1,2);
+        for it = trk(idxT).start:trk(idxT).end
+            itf = find(T{it}(:,3)==idxT);
+            if ~isempty(itf)
+                XY(it-trk(idxT).start+1,:) = round(T{it}(itf(1),1:2)+.5);
+            end
+        end
+        E = cell(1,nCh);
+        for ich = 1:nCh
+            E{ich} = zeros([rCCP*2+1,rCCP*2+1,size(XY,1)],'single');
+            for it = 1:size(XY,1)
+                if XY(it,1)>=0
+                    tmpI = padarray(I{2,ich}(:,:,it+trk(idxT).start-1),rCCP*[1,1]);
+                    tmpI = tmpI * BC(it+trk(idxT).start-1,ich);
+                    E{ich}(:,:,it) = tmpI(XY(it,2)+(0:2*rCCP),XY(it,1)+(0:2*rCCP));
+                end
+            end
+            saveTiff(E{ich},[folder 'Tracking' filesep 'Track_' num2str(idxT) '_' num2str(wl(ich)) '.tif']);
+        end
+        expTrk(idxT).status = 1;
+    end
+end
+end
